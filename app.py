3ced1dda0454c728ac6eb065538e09a733780433
aman-sharma7m <aman.7m@outlook.com>
2024-02-05 04:39:59 +0530

    project done

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 import streamlit as st 
 from dotenv import load_dotenv
+from uuid import uuid4
+from utils import *
 
 #loading the keys 
 load_dotenv()
 
+#session_state
+if 'unique_key' not in st.session_state:
+  st.session_state['unique_key']=''
+
 
 def main():
   st.set_page_config(page_title='hr-asist-tool')
-  st.title('🙎‍♀️HR-Resume Screening Assistance')
+  st.title('🙎‍♀️ HR-Resume Screening Assistance')
   st.subheader('I can help you in resume screening progress')
 
   job_desc=st.text_area("Please paste your 'JOB DESCRIPTION' here")
@@ -18,7 +24,30 @@ def main():
 
 
   if button:
-    pass
+    with st.spinner('Wait for it........'):
+      #getting unique id so that each session only uploaded file will be used to select the data
+      st.session_state['unique_key']=uuid4().hex
+
+      #sending the files to create docs
+      docs=create_docs(files,st.session_state['unique_key'])
+      st.write('creating the docs......')
+
+      #get embeddings
+      st.write('getting embeddings......')
+      embeddings=get_embeddings()
+      
+
+      #store to pincone
+      st.write('storing the docs......')
+      store_to_pinecone(docs,embeddings)
+      
+
+      #retrieval of docs
+      st.write('retrieve relevant docs......')
+      relevant_docs=get_docs_from_pinecone(job_desc,num_count,embeddings,st.session_state['unique_key'])
+
+      st.write(relevant_docs)
+
 
 
 if __name__=='__main__':
---
+++ utils.py
@@ -0,0 +1,62 @@
+import streamlit as st
+import PyPDF2
+from langchain.docstore.document import Document
+from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+from langchain.vectorstores.pinecone import Pinecone
+from pinecone import Pinecone as pc, PodSpec
+
+
+def extract_text(pdf_file):
+  pdf_obj=PyPDF2.PdfReader(pdf_file)
+  text=''
+  for page in pdf_obj.pages:
+    data=page.extract_text()
+    text+=data
+  return text
+
+def create_docs(pdf_list,unique_id):
+  docs=[]
+  for file in pdf_list:
+    text=extract_text(file)
+    docs.append(Document(page_content=text,metadata={'name':file.name,
+                                                     'size':file.size,
+                                                     'type':file.type,
+                                                     'id':file.file_id,
+                                                     'unique_id':unique_id}))
+  return docs
+
+
+def get_embeddings():
+  return SentenceTransformerEmbeddings(model_name='all-MiniLM-L6-V2')
+
+
+def store_to_pinecone(docs,embeddings):
+  pc_config=pc()
+  index_name='hr-assist'
+  for name in pc_config.list_indexes().names():
+    if name!=index_name:
+      try:
+        pc_config.delete_index(name)
+        print(f'delete index {index_name}')
+      except Exception as e:
+        print('no index is there')
+
+  if pc_config.list_indexes().names()==[]:
+    print('creating new index')
+    pc_config.create_index(name=index_name,
+                    dimension=embeddings.client.get_sentence_embedding_dimension(),
+                    metric='dotproduct',
+                    spec=PodSpec(environment='gcp-starter'))
+  
+  Pinecone.from_documents(docs,embeddings,index_name=index_name)
+
+
+def get_docs_from_pinecone(query,num_count,embeddings,u_id):
+  index_name='hr-assist'
+  index=Pinecone.from_existing_index(index_name=index_name,embedding=embeddings)
+  return index.similarity_search_with_score(query,int(num_count),{'unique_id':u_id})
+
+
+
+
+
